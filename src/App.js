a337479ff2585f99355348d9f0a9fe55ec6e3565
Traian Alexandru <victor_traian@yahoo.com>
2020-03-21 23:47:49 -0400

    before eject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Navigation from './layout/Navigation';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navigation />
+        <header className="App-header">
+          <Switch>
+            <Redirect from="/home" to="/" />
+            <Route path="/" component={Page1} exact />
+            <Route path="/page2" component={Page2} exact />
+          </Switch>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Page2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Page2.css';
+
+export default class Page2 extends Component {
+  render() {
+    return (
+      <div className="test">
+        Page2
+      </div>
+    )
+  }
+}
