1ede1d9657468b4baa50b46719f21dfa2fd20ff8
Traian Alexandru <victor_traian@yahoo.com>
2021-06-05 21:23:04 -0400

    flash error handlink on todo

--- src/apps/todos/useLocalStorage/TodoLocalForm.js
+++ src/apps/todos/useLocalStorage/TodoLocalForm.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Col, Form } from 'react-bootstrap';
+import { Alert, Button, Col, Form } from 'react-bootstrap';
 import { useLocalStorage } from './useLocalStorage';
 
 const getRandomId = () => Math.random();
@@ -7,12 +7,19 @@ const getRandomId = () => Math.random();
 const TodoLocalForm = ({ isEdit, setIsEdit }) => {
   const { todos, createTodo, updateTodo } = useLocalStorage();
   const [title, setTitle] = useState('');
+  const [showError, setShowError] = useState(false);
+
+  const displayError = () => {
+    setShowError(true);
+    const clearTimer = setTimeout(() => setShowError(false), 3000);
+    return () => clearTimeout(clearTimer);
+  };
 
   const onCreateTodo = (e) => {
     e.preventDefault();
 
     if (title === '') {
-      alert('Fill the form');
+      displayError();
       return;
     }
 
@@ -37,28 +44,35 @@ const TodoLocalForm = ({ isEdit, setIsEdit }) => {
   }, [isEdit]);
 
   return (
-    <Form
-      className="d-flex justify-content-center align-items-center mt-4"
-      onSubmit={onCreateTodo}
-    >
-      <Form.Group
-        as={Col}
-        controlId="text"
-        style={{ marginBottom: 0, paddingLeft: 0 }}
+    <>
+      <Form
+        className="d-flex justify-content-center align-items-center mt-4"
+        onSubmit={onCreateTodo}
       >
-        <Form.Control
-          type="text"
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-          value={title}
-          placeholder="Things you wanna do..."
-          required
-        />
-      </Form.Group>
-      <Button type="submit" variant="light" className="todo-item">
-        Save
-      </Button>
-    </Form>
+        <Form.Group
+          as={Col}
+          controlId="text"
+          style={{ marginBottom: 0, paddingLeft: 0 }}
+        >
+          <Form.Control
+            type="text"
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+            value={title}
+            placeholder="Things you wanna do..."
+            required
+          />
+        </Form.Group>
+        <Button type="submit" variant="light" className="todo-item">
+          Save
+        </Button>
+      </Form>
+      {showError && (
+        <Alert variant="danger">
+          <Alert.Heading>Please enter a todo!</Alert.Heading>
+        </Alert>
+      )}
+    </>
   );
 };
 
